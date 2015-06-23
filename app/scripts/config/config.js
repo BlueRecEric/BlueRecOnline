4b2b4763abf72db9d3e109c7efd42e8e24cde3a4
BlueRecEric <eric@bluerec.com>
2015-06-23 16:04:28 -0500

    Revert "Test"
    This reverts commit 683049c9e68effb616e7de4326f93f3afd47e50b.

--- app/scripts/config/config.js
+++ app/scripts/config/config.js
@@ -1,6 +1,6 @@
 angular.module('bluereconlineApp.apiconfig', []);
 
 angular.module('bluereconlineApp.apiconfig').constant('BLUEREC_ONLINE_CONFIG', {
-  'API_URL': 'https://alphadev.bluerec.com/php/API',
+  'API_URL': 'YOUR_API_URL',
   'API_KEY': 'YOUR_API_KEY'
 });
