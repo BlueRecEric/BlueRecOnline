913a151847030f489912b687a87984da4d2812a8
BlueRecPeter <peter@bockyn.com>
2015-06-26 04:12:08 -0500

    shoppingcart

--- app/scripts/controllers/menuCtrl.js
+++ app/scripts/controllers/menuCtrl.js
@@ -8,46 +8,65 @@
  * Controller of the bluereconlineApp
  */
 angular.module('bluereconlineApp')
-  .controller('MenuCtrl', ['$scope', '$routeParams', '$route', 'AuthService', '$location', 'ActiveUser', function ($scope,$routeParams,$route,AuthService,$location,ActiveUser) {
+    .controller('MenuCtrl', ['$scope', '$routeParams', '$route', 'AuthService', '$location', 'ActiveUser', '$aside', function ($scope,$routeParams,$route,AuthService,$location,ActiveUser, $aside) {
 
+        ActiveUser.getFromLocal().then(function success(response) {
+            console.log('we got the user from the menu');
+            $scope.currentUser = response;
+            console.log($scope.currentUser);
+            //$scope.$root.currentUser = response.data;
+        });
 
-    $scope.loggedIn=false;
+        $scope.$watch(function() { return ActiveUser.getUser(); }, function() {
+            console.log('ActiveUser changed');
+            $scope.currentUser = ActiveUser.getUser();
 
+            $scope.loggedIn=$scope.currentUser.validLogin;
 
-    ActiveUser.getFromLocal().then(function success(response) {
-      console.log('we got the user from the menu');
-      $scope.currentUser = response;
-      console.log($scope.currentUser);
-      //$scope.$root.currentUser = response.data;
-    });
+            //console.table($scope.currentUser );
+        });
 
-    $scope.$watch(function() { return ActiveUser.getUser(); }, function() {
-      console.log('ActiveUser changed');
-      $scope.currentUser = ActiveUser.getUser();
-    });
+        //console.log('current params:');
+        //console.log($routeParams);
 
-    //console.log('current params:');
-    //console.log($routeParams);
+        if(angular.isDefined($routeParams.current)) {
+            $scope.ActivitiesLink = $routeParams.current.params.orgurl + '/programs/';
+        }
 
-    if(angular.isDefined($routeParams.current)) {
-      $scope.ActivitiesLink = $routeParams.current.params.orgurl + '/programs/';
-    }
+        $scope.$on('$routeChangeSuccess', function() {
+            //console.log('change params:');
+            //console.log($routeParams);
+            $scope.ActivitiesLink = $routeParams.orgurl + '/programs/';
+            $scope.MembershipsLink = $routeParams.orgurl + '/memberships/';
+            $scope.ReservationsLink = $routeParams.orgurl + '/reservations/';
+            $scope.ShopLink = $routeParams.orgurl + '/shop/';
+            $scope.LoginLink = $routeParams.orgurl + '/login/';
+            $scope.HomeLink = $routeParams.orgurl + '/home/';
+        });
 
-    $scope.$on('$routeChangeSuccess', function() {
-      //console.log('change params:');
-      //console.log($routeParams);
-      $scope.ActivitiesLink = $routeParams.orgurl + '/programs/';
-      $scope.MembershipsLink = $routeParams.orgurl + '/memberships/';
-      $scope.ReservationsLink = $routeParams.orgurl + '/reservations/';
-      $scope.ShopLink = $routeParams.orgurl + '/shop/';
-      $scope.LoginLink = $routeParams.orgurl + '/login/';
-      $scope.HomeLink = $routeParams.orgurl + '/home/';
-    });
+        $scope.logout = function()
+        {
+            $scope.loggedIn=false;
 
-    $scope.logout = function()
-    {
-      AuthService.logout();
-      $scope.currentUser = {};
-      $location.path('/' + $routeParams.orgurl + '/login');
-    };
-  }]);
+            AuthService.logout();
+            $scope.currentUser = {};
+            $location.path('/' + $routeParams.orgurl + '/login');
+        };
+
+        $scope.openAside = function openAside() {
+
+            // Pre-fetch an external template populated with a custom scope
+            var myOtherAside = $aside({scope: $scope, template: 'shoppingcart.html'});
+            // Show when some event occurs (use $promise property to ensure the template has been loaded)
+            myOtherAside.$promise.then(function() {
+                myOtherAside.show();
+            });
+
+        };
+
+        $scope.popover = {
+            'title': 'No Items',
+            'content': 'Your cart is empty!!'
+        };
+
+    }]);
