8f4154709224bf91f37c70627922a04911d64bc0
BlueRecPeter <peter@bockyn.com>
2015-06-29 07:04:29 -0500

    a

--- app/scripts/sitescripts.js
+++ app/scripts/sitescripts.js
@@ -5,21 +5,21 @@
 
 $(document).ready(function () {
 
-  /*var menu = $('.bluerecnav');
-  var origOffsetY = menu.offset().top + 0;
+    var menu = $('.bluerecnav');
+     var origOffsetY = menu.offset().top + 0;
 
-  //console.log('Offset:' + origOffsetY);
+     //console.log('Offset:' + origOffsetY);
 
-  function scroll() {
-    if ($(window).scrollTop() > origOffsetY) {
-        $('.bluerecnavhidden').addClass('bluerecnavhidden2');
+     function scroll() {
+     if ($(window).scrollTop() > origOffsetY) {
+     $('.bluerecnavhidden').addClass('bluerecnavhidden2');
 
-    } else {
-        $('.bluerecnavhidden').removeClass('bluerecnavhidden2');
-    }
+     } else {
+     $('.bluerecnavhidden').removeClass('bluerecnavhidden2');
+     }
 
-  }
+     }
 
-  document.onscroll = scroll;*/
+     document.onscroll = scroll;
 
 });
