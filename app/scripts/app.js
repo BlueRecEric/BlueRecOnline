ee1a76cffe72281fb782645f2bf9b94a57960333
Peter <peter@bockyn.com>
2017-10-13 09:32:53 -0500

    Revert "Revert "Revert "f"""
    This reverts commit 85889ba13a65fbe0e7ea67b471ddf288fd4dc319.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1133,7 +1133,7 @@ angular
     $scope.$on('$routeChangeSuccess', function() {
         $scope.CssLink = $routeParams.orgurl;
 
-        $scope.headerLogo = '';d
+        $scope.headerLogo = '';
 
         isImage('images/'+$routeParams.orgurl+'.png').then(function(test) {
             if(test)
